0e8721002f37d23c5eafe9f0d5d7641d28d50566
dzqdzq <635587322@qq.com>
2023-08-14 00:50:49 +0800

    修复BUG，以及实时更新uuid

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,23 @@
 import * as vscode from 'vscode';
-const fs = vscode.workspace.fs;
+import * as fs from 'fs';
+const vscodeFs = vscode.workspace.fs;
 
 let lineInfos: number[] | null = null;
 let libraryInfos: { [key: string]: { relativePath: string } } = {};
 let isHaveLibrary = false;
-let importsPath: string | null = null;
+let importsPath: string = '';
+let dealLibraryLock = false;
+const decorationType = vscode.window.createTextEditorDecorationType({
+  rangeBehavior: vscode.DecorationRangeBehavior.ClosedClosed,
+  overviewRulerLane: vscode.OverviewRulerLane.Full,
+  after: {
+    color: '#808080',
+  },
+});
 
 async function getSpriteFrame(uuid: string) {
   const jsonPath = `${importsPath}/${uuid.substring(0, 2)}/${uuid}.json`;
-  const data = await fs.readFile(vscode.Uri.file(jsonPath));
+  const data = await vscodeFs.readFile(vscode.Uri.file(jsonPath));
   if (data.length <= 1) {
     return '';
   }
@@ -119,109 +128,153 @@ function getDecoration(
   }
 }
 
-async function dealEditor(editor?: vscode.TextEditor): Promise<void> {
-  editor = editor || vscode.window.activeTextEditor;
-  if (editor) {
-    const document = editor.document;
+function resetLibraryInfos() {
+  isHaveLibrary = false;
+  libraryInfos = {};
+  importsPath = '';
+}
 
-    if (document.languageId !== 'prefab') {
-      return;
-    }
-    const line = document.lineAt(2).text.trim();
-    if (
-      line !== '"__type__": "cc.Prefab",' &&
-      line !== '"__type__": "cc.SceneAsset",'
-    ) {
-      return;
-    }
-    preLineInfos(document);
+function setLibraryInfos(data: Uint8Array) {
+  try {
+    libraryInfos = JSON.parse(data.toString());
+    isHaveLibrary = true;
+  } catch (error) {
+    resetLibraryInfos();
+  }
+  return isHaveLibrary;
+}
 
-    if (!isHaveLibrary) {
-      return;
-    }
-
-    // 应用装饰
-    let decorationType = vscode.window.createTextEditorDecorationType({
-      rangeBehavior: vscode.DecorationRangeBehavior.ClosedClosed,
-      overviewRulerLane: vscode.OverviewRulerLane.Full,
-      after: {
-        color: '#808080',
-      },
-    });
-    let decorationOptions: vscode.DecorationOptions[] = [];
-
-    for (let i = 1, c = document.lineCount; i < c; i++) {
-      let line = document.lineAt(i).text;
-      if (line.length < 42) {
-        continue;
+async function loadUuidJson(jsonPath: string) {
+  const jsonUri = vscode.Uri.file(jsonPath);
+  try {
+    const data = await vscodeFs.readFile(jsonUri);
+    importsPath = jsonPath.replace('uuid-to-mtime.json', 'imports');
+    fs.watch(jsonPath, (eventType: fs.WatchEventType, filename) => {
+      if (eventType === 'change') {
+        const jsonUri = vscode.Uri.file(jsonPath);
+        vscodeFs.readFile(jsonUri).then((data) => {
+          setLibraryInfos(data);
+          importsPath = jsonPath.replace('uuid-to-mtime.json', 'imports');
+        }, resetLibraryInfos);
       }
-      let uuidIdx = line.indexOf('"__uuid__":');
-      if (uuidIdx > 0) {
-        const uuid = line.substring(uuidIdx + 13, uuidIdx + 49);
-        const asset = libraryInfos[uuid];
-        if (asset) {
+
+      console.log(`File ${filename} has been ${eventType}`);
+    });
+    setLibraryInfos(data);
+  } catch (error) {
+    resetLibraryInfos();
+  }
+  return isHaveLibrary;
+}
+
+async function dealLibrary(): Promise<void> {
+  if (dealLibraryLock) {
+    return;
+  }
+
+  dealLibraryLock = true;
+  do {
+    let workspaceFolders = vscode.workspace.workspaceFolders;
+    if (!workspaceFolders) {
+      break;
+    }
+
+    if (isHaveLibrary) {
+      break;
+    }
+
+    for (let workspaceFolder of workspaceFolders) {
+      let workspaceFolderPath = workspaceFolder.uri.fsPath;
+      const jsonPath = workspaceFolderPath + '/library/uuid-to-mtime.json';
+      if (await loadUuidJson(jsonPath)) {
+        break;
+      }
+    }
+    // eslint-disable-next-line no-constant-condition
+  } while (false);
+  dealLibraryLock = false;
+}
+
+async function dealEditor(): Promise<void> {
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return;
+  }
+
+  // 插件正在忙
+  if (dealLibraryLock) {
+    return;
+  }
+
+  const document = editor.document;
+
+  if (document.languageId !== 'prefab') {
+    return;
+  }
+  const line = document.lineAt(2).text.trim();
+  if (
+    line !== '"__type__": "cc.Prefab",' &&
+    line !== '"__type__": "cc.SceneAsset",'
+  ) {
+    return;
+  }
+  preLineInfos(document);
+
+  if (!isHaveLibrary) {
+    await dealLibrary();
+  }
+
+  if (!isHaveLibrary) {
+    return;
+  }
+
+  // 应用装饰
+  let decorationOptions: vscode.DecorationOptions[] = [];
+
+  for (let i = 1, c = document.lineCount; i < c; i++) {
+    let line = document.lineAt(i).text;
+    if (line.length < 42) {
+      continue;
+    }
+    let uuidIdx = line.indexOf('"__uuid__":');
+    if (uuidIdx > 0) {
+      const uuid = line.substring(uuidIdx + 13, uuidIdx + 49);
+      const asset = libraryInfos[uuid];
+      if (asset) {
+        let positionStart = new vscode.Position(i, uuidIdx + 52);
+        let range = new vscode.Range(positionStart, positionStart);
+        let decoration: vscode.DecorationOptions = {
+          range: range,
+          renderOptions: {
+            after: { contentText: ' ' + asset.relativePath },
+          },
+        };
+        decorationOptions.push(decoration);
+      } else if (document.lineAt(i - 1).text.indexOf('"_spriteFrame":') > 0) {
+        const contentText = await getSpriteFrame(uuid);
+        if (contentText) {
           let positionStart = new vscode.Position(i, uuidIdx + 52);
           let range = new vscode.Range(positionStart, positionStart);
           let decoration: vscode.DecorationOptions = {
             range: range,
-            renderOptions: {
-              after: { contentText: ' ' + asset.relativePath },
-            },
+            renderOptions: { after: { contentText: ' ' + contentText } },
           };
           decorationOptions.push(decoration);
-        } else if (document.lineAt(i - 1).text.indexOf('"_spriteFrame":') > 0) {
-          const contentText = await getSpriteFrame(uuid);
-          if (contentText) {
-            let positionStart = new vscode.Position(i, uuidIdx + 52);
-            let range = new vscode.Range(positionStart, positionStart);
-            let decoration: vscode.DecorationOptions = {
-              range: range,
-              renderOptions: { after: { contentText: ' ' + contentText } },
-            };
-            decorationOptions.push(decoration);
-          }
         }
-      } else if ((uuidIdx = line.indexOf('"__type__":')) > 0) {
-        const uuid = line.substring(uuidIdx + 13, uuidIdx + 36);
-        getDecoration(i, uuid, decorationOptions);
-      } else if ((uuidIdx = line.indexOf('"_componentId":')) > 0) {
-        const uuid = line.substring(uuidIdx + 17, uuidIdx + 40);
-        getDecoration(i, uuid, decorationOptions);
       }
+    } else if ((uuidIdx = line.indexOf('"__type__":')) > 0) {
+      const uuid = line.substring(uuidIdx + 13, uuidIdx + 36);
+      getDecoration(i, uuid, decorationOptions);
+    } else if ((uuidIdx = line.indexOf('"_componentId":')) > 0) {
+      const uuid = line.substring(uuidIdx + 17, uuidIdx + 40);
+      getDecoration(i, uuid, decorationOptions);
     }
-
-    editor.setDecorations(decorationType, decorationOptions);
   }
+
+  editor.setDecorations(decorationType, decorationOptions);
 }
 
 export async function activate(context: vscode.ExtensionContext) {
-  isHaveLibrary = false;
-  // 获取当前路径
-  let workspaceFolders = vscode.workspace.workspaceFolders;
-  if (!workspaceFolders) {
-    return;
-  }
-
-  for (let workspaceFolder of workspaceFolders) {
-    let workspaceFolderPath = workspaceFolder.uri.fsPath;
-    const jsonPath = workspaceFolderPath + '/library/uuid-to-mtime.json';
-    const jsonUri = vscode.Uri.file(jsonPath);
-
-    try {
-      const data = await fs.readFile(jsonUri);
-      if (data.length <= 1) {
-        continue;
-      }
-
-      libraryInfos = JSON.parse(data.toString());
-      importsPath = workspaceFolderPath + '/library/imports';
-      isHaveLibrary = true;
-      break;
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
   context.subscriptions.push(
     vscode.languages.registerDefinitionProvider('prefab', {
       provideDefinition(document, position) {
@@ -251,7 +304,7 @@ export async function activate(context: vscode.ExtensionContext) {
 
   // 注册一个命令，用于解析 JSON 文件并创建对应的符号
   context.subscriptions.push(
-    vscode.workspace.onDidChangeTextDocument(() => dealEditor()),
+    vscode.commands.registerCommand('creatorPrefab.parsePrefab', dealEditor),
     vscode.window.onDidChangeActiveTextEditor(dealEditor)
   );
 
